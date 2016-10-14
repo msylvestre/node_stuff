5291d9666bf3299e4ebe34e42f890e88b8707442
Marco Sylvestre <marco@venzee.com>
2016-10-14 08:50:16 -0400

    added protractor example

---
+++ conf.js
@@ -0,0 +1,6 @@
+// conf.js
+exports.config = {
+  framework: 'jasmine',
+  seleniumServerJar: "./node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-2.53.1.jar",
+  specs: ['spec.js']
+}
