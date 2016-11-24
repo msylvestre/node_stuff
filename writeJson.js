995bd18914db26f2c79a664b851cb53245721c96
Marco Sylvestre <marco@venzee.com>
2016-11-24 18:56:55 -0500

    modified example

--- writeJson.js
+++ writeJson.js
@@ -1,16 +1,38 @@
+
+// Very good reference : https://www.codementor.io/nodejs/tutorial/how-to-use-json-files-in-node-js
+
+
 var fs = require('fs');
 var fileName = './file.json';
 var file = require(fileName);
 
-file.test =  {
-  "id": 5,
-  "environement": "dev",
-  "url": "https://test.com",
+myApp = "BigCommerce";
+
+file[myApp] =  {
+  "id": 6,
+  "environement": "prod",
+  "url": "https://bc.com",
   "user": {
     "email": "tbd",
     "password": "<secret>"
   }
 };
+
+/*
+Or if you know the name of the object to add to to the file you can use : 
+
+file.BigCommerce =  {
+  "id": 6,
+  "environement": "prod",
+  "url": "https://bc.com",
+  "user": {
+    "email": "tbd",
+    "password": "<secret>"
+  }
+};
+
+
+*/
 
 // Display the "user" property of the object "venzee" from the json file.  It seems a "require" also load the file in memory, magie :D
 console.log("------------------------------------------------\nGet the property file.venzee.user")
